54fbb7193bb27ac13ad7cf455747e0f9476ea7e1
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-05-09 09:57:59 -0400

    setting3

--- lm_eval/models/huggingface.py
+++ lm_eval/models/huggingface.py
@@ -570,9 +570,10 @@ class HFLM(TemplateLM):
             
             # loadingfromcheckpoint = "/home/yangzho6/model_checkpoints/smallmodelkernelsize2setting0checkpoint-1000" 
             # loadingfromcheckpoint = "/home/yangzho6/model_checkpoints/tinyllamasmallmodelkernelsize2setting0oldcheckpoint-3000" 
-            loadingfromcheckpoint = "/home/yangzho6/model_checkpoints/smallmodelkernelsize4setting0checkpoint-1000" 
-            kernel_size = 4 
-            experiment_setting = "setting0" 
+            # loadingfromcheckpoint = "/home/yangzho6/model_checkpoints/smallmodelkernelsize4setting0checkpoint-1000" 
+            loadingfromcheckpoint = "/home/yangzho6/model_checkpoints/tinyllama_setting3_kernelsize2_2_checkpoint-1900" 
+            kernel_size = 2 
+            experiment_setting = "setting3" 
             
             self._model = LlamaWeirdLargeTest.from_pretrained(loadingfromcheckpoint) 
             self._model.set_sliding_window_length(kernel_size) 
