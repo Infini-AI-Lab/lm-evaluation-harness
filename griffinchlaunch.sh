8601038de990314f8e55e25bf6ce8078ce0ee192
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-05-14 14:22:08 -0700

    differentrunningscript

--- griffinchlaunch.sh
+++ griffinchlaunch.sh
@@ -1,1 +1,2 @@
-lm_eval --model hf --model_args pretrained=meta-llama/Llama-2-7b-hf_griffin --tasks gsm8k --device cuda:0 --batch_size 1 --limit 0.2 
+# lm_eval --model hf --model_args pretrained=meta-llama/Llama-2-7b-hf_griffin --tasks gsm8k --device cuda:0 --batch_size 1 --limit 0.2 
+lm_eval --model hf --model_args pretrained=meta-llama/Llama-2-7b-hf_chunked_8 --tasks gsm8k --device cuda:0 --batch_size 1 --limit 0.2 
