ea5be1f3779365a8033742e683c1482870392ef2
dreaming-panda <chenzhuoming911@gmail.com>
2024-06-08 01:49:11 +0000

    lm

--- lm_eval/models/specdec.py
+++ lm_eval/models/specdec.py
@@ -762,7 +762,7 @@ class SDLM(TemplateLM):
             #tokens = self._model.generate(inputs=input_ids, do_sample=True, temperature=T, top_k=32000, top_p=1.0, max_new_tokens=max_gen_toks)
             
             num_samples = tokens.shape[1] - initial_len
-            acceptance_rate = torch.zeros(self.width).to(self.device)
+            
             
             target_logits :torch.Tensor= self._model(input_ids=tokens).logits
             draft_logits :torch.Tensor= self._draft(input_ids=tokens).logits
@@ -772,40 +772,15 @@ class SDLM(TemplateLM):
                 
             target_logits = target_logits[...,initial_len:,:]
             draft_logits = draft_logits[...,initial_len:,:]
-            target_proba = torch.nn.functional.softmax(target_logits/T, dim=-1)[0]
-            draft_proba = torch.nn.functional.softmax(draft_logits/T, dim=-1)[0]
+            target_proba = torch.nn.functional.softmax(target_logits/T, dim=-1).unsqueeze(-1)
+            draft_proba = torch.nn.functional.softmax(draft_logits/T, dim=-1).unsqueeze(-1)
             
-            for i in range(target_proba.shape[0]):
-                token_acceptance_rate = torch.zeros(self.width).to(self.device)
-                
-                
-                token_target_prob = target_proba[i]
-                # token_draft_prob = q[i]
-                #draft_model_prob.append(q[i].cpu())
-                token_draft_logits = draft_logits[0][i]
-
-                token_draft_prob = F.softmax(token_draft_logits / T, dim=-1).squeeze(0)
-                sampled_token = token_draft_prob.multinomial(num_samples=1, replacement=True)
-                
-                
-                token_acceptance_rate[0] = min(1.0, (token_target_prob[sampled_token]/ token_draft_prob[sampled_token]))
-
-                token_target_prob = get_residual(token_target_prob, token_draft_prob)
-                
-                
-                for j in range(self.width-1):
-                    token_draft_logits[sampled_token] = - torch.inf
-                    token_draft_prob = F.softmax(token_draft_logits / (T), dim=-1).squeeze(0)
-                    if torch.isnan(token_draft_prob).long().sum() >= 1:
-                        break
-                    token_draft_prob = token_draft_prob / token_draft_prob.sum(-1)
-                    sampled_token = token_draft_prob.multinomial(num_samples=1, replacement=True)
-                    branch_token_acceptance_rate = min(1, token_target_prob[sampled_token]/ token_draft_prob[sampled_token])
-                    token_acceptance_rate[j+1] = (1 - token_acceptance_rate.sum()) * branch_token_acceptance_rate
-                    
-                    token_target_prob = get_residual(token_target_prob, token_draft_prob)
-                acceptance_rate = acceptance_rate + token_acceptance_rate
+            probas = torch.cat([target_proba, draft_proba], dim=-1)
+            probas = torch.min(probas, dim=-1).values
+            acceptance_rate = probas.sum(dim=-1)
+        
+            total_acceptance_rate = acceptance_rate.sum(dim=-1)
                  
+
         
-        
-        return acceptance_rate.cpu()/num_samples, num_samples
+        return total_acceptance_rate.cpu()/num_samples, num_samples
--- test_truthfulqagen.sh
+++ test_truthfulqagen.sh
@@ -1,40 +1,40 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model sd \
-    --model_args pretrained=meta-llama/Llama-2-70b-hf,draft=meta-llama/Llama-2-7b-hf,temperature=0.6,width=1\
+CUDA_VISIBLE_DEVICES=1,2,3,4 lm_eval --model sd \
+    --model_args pretrained=meta-llama/Llama-2-70b-chat-hf,draft=meta-llama/Llama-2-7b-chat-hf,temperature=0.6,width=1\
     --tasks truthfulqa_gen\
-    --device cuda:0,1,2,3,4 \
+    --device cuda:1,2,3,4 \
     --batch_size 1 \
     --num_fewshot 0 \
     --limit 0.1 \
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model sd \
-    --model_args pretrained=meta-llama/Llama-2-70b-hf,draft=meta-llama/Llama-2-7b-hf,temperature=0.6,width=1\
+CUDA_VISIBLE_DEVICES=1,2,3,4 lm_eval --model sd \
+    --model_args pretrained=meta-llama/Llama-2-70b-chat-hf,draft=meta-llama/Llama-2-7b-chat-hf,temperature=0.6,width=1\
     --tasks truthfulqa_gen\
-    --device cuda:0,1,2,3,4 \
+    --device cuda:1,2,3,4 \
     --batch_size 1 \
     --num_fewshot 1 \
     --limit 0.1 \
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model sd \
-    --model_args pretrained=meta-llama/Llama-2-70b-hf,draft=meta-llama/Llama-2-7b-hf,temperature=0.6,width=1\
+CUDA_VISIBLE_DEVICES=1,2,3,4 lm_eval --model sd \
+    --model_args pretrained=meta-llama/Llama-2-70b-chat-hf,draft=meta-llama/Llama-2-7b-chat-hf,temperature=0.6,width=1\
     --tasks truthfulqa_gen\
-    --device cuda:0,1,2,3,4 \
+    --device cuda:1,2,3,4 \
     --batch_size 1 \
     --num_fewshot 2 \
     --limit 0.1 \
 
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model sd \
-    --model_args pretrained=meta-llama/Llama-2-70b-hf,draft=meta-llama/Llama-2-7b-hf,temperature=0.6,width=1\
+CUDA_VISIBLE_DEVICES=1,2,3,4 lm_eval --model sd \
+    --model_args pretrained=meta-llama/Llama-2-70b-chat-hf,draft=meta-llama/Llama-2-7b-chat-hf,temperature=0.6,width=1\
     --tasks truthfulqa_gen\
-    --device cuda:0,1,2,3,4 \
+    --device cuda:1,2,3,4 \
     --batch_size 1 \
     --num_fewshot 3 \
     --limit 0.1 \
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model sd \
-    --model_args pretrained=meta-llama/Llama-2-70b-hf,draft=meta-llama/Llama-2-7b-hf,temperature=0.6,width=1\
+CUDA_VISIBLE_DEVICES=1,2,3,4 lm_eval --model sd \
+    --model_args pretrained=meta-llama/Llama-2-70b-chat-hf,draft=meta-llama/Llama-2-7b-chat-hf,temperature=0.6,width=1\
     --tasks truthfulqa_gen\
-    --device cuda:0,1,2,3,4 \
+    --device cuda:1,2,3,4 \
     --batch_size 1 \
     --num_fewshot 5 \
     --limit 0.1 \
