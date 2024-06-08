6302eb069ef8e1fa1e45ca599049273442785444
Zhuoming Chen <zhuominc@lovelace.ece.local.cmu.edu>
2024-06-08 01:01:23 -0400

    fewshot examples

--- test_gsm8k.sh
+++ test_gsm8k.sh
@@ -1,40 +1,40 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model sd \
-    --model_args pretrained=meta-llama/Llama-2-70b-hf,draft=meta-llama/Llama-2-7b-hf,temperature=0.6,width=1\
+CUDA_VISIBLE_DEVICES=0 lm_eval --model sd \
+    --model_args pretrained=meta-llama/Llama-2-7b-hf,draft=princeton-nlp/Sheared-LLaMA-1.3B,temperature=0.6,width=1\
     --tasks gsm8k\
-    --device cuda:0,1,2,3,4 \
+    --device cuda:0 \
     --batch_size 1 \
     --num_fewshot 0 \
     --limit 0.1 \
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model sd \
-    --model_args pretrained=meta-llama/Llama-2-70b-hf,draft=meta-llama/Llama-2-7b-hf,temperature=0.6,width=1\
+CUDA_VISIBLE_DEVICES=0 lm_eval --model sd \
+    --model_args pretrained=meta-llama/Llama-2-7b-hf,draft=princeton-nlp/Sheared-LLaMA-1.3B,temperature=0.6,width=1\
     --tasks gsm8k\
-    --device cuda:0,1,2,3,4 \
+    --device cuda:0 \
     --batch_size 1 \
     --num_fewshot 1 \
     --limit 0.1 \
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model sd \
-    --model_args pretrained=meta-llama/Llama-2-70b-hf,draft=meta-llama/Llama-2-7b-hf,temperature=0.6,width=1\
+CUDA_VISIBLE_DEVICES=0 lm_eval --model sd \
+    --model_args pretrained=meta-llama/Llama-2-7b-hf,draft=princeton-nlp/Sheared-LLaMA-1.3B,temperature=0.6,width=1\
     --tasks gsm8k\
-    --device cuda:0,1,2,3,4 \
+    --device cuda:0 \
     --batch_size 1 \
     --num_fewshot 2 \
     --limit 0.1 \
 
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model sd \
-    --model_args pretrained=meta-llama/Llama-2-70b-hf,draft=meta-llama/Llama-2-7b-hf,temperature=0.6,width=1\
+CUDA_VISIBLE_DEVICES=0 lm_eval --model sd \
+    --model_args pretrained=meta-llama/Llama-2-7b-hf,draft=princeton-nlp/Sheared-LLaMA-1.3B,temperature=0.6,width=1\
     --tasks gsm8k\
-    --device cuda:0,1,2,3,4 \
+    --device cuda:0 \
     --batch_size 1 \
     --num_fewshot 3 \
     --limit 0.1 \
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4 lm_eval --model sd \
-    --model_args pretrained=meta-llama/Llama-2-70b-hf,draft=meta-llama/Llama-2-7b-hf,temperature=0.6,width=1\
+CUDA_VISIBLE_DEVICES=0 lm_eval --model sd \
+    --model_args pretrained=meta-llama/Llama-2-7b-hf,draft=princeton-nlp/Sheared-LLaMA-1.3B,temperature=0.6,width=1\
     --tasks gsm8k\
-    --device cuda:0,1,2,3,4 \
+    --device cuda:0 \
     --batch_size 1 \
     --num_fewshot 5 \
     --limit 0.1 \
